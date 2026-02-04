fc793023c7a3c64190e2e1c202b602138a9bb21f
Se2one <sh@mirny.io>
2026-02-04 22:58:50 +0900

    feat: add average memory usage calculation using vmstat free/buff/cache

--- shell.sh
+++ shell.sh
@@ -9,15 +9,34 @@ vmstat $INTERVAL $COUNT > vmstat.txt
 cpu_us=$(awk '{print $13}' vmstat.txt)
 cpu_sy=$(awk '{print $14}' vmstat.txt)
 
+mem_free=$(awk '{print $4}' vmstat.txt)
+mem_buff=$(awk '{print $5}' vmstat.txt)
+mem_cache=$(awk '{print $6}' vmstat.txt)
+
+
 sum_us=0
 sum_sy=0
+sum_free=0
+sum_buff=0
+sum_cache=0
 
 first_us=true
 second_us=true
 
+first_sy=true
+second_sy=true
+
+first_free=true
+second_free=true
+
+first_buff=true
+second_buff=true
+
+first_cache=true
+second_cache=true
+
 for v in $cpu_us
 do
-	cnt_us=$((cnt_us + 1))
 
 	if $first_us; then # 첫 번쨰 us 값 제외
 		first_us=false
@@ -27,6 +46,8 @@ do
 		second_us=false
 		continue
 	fi
+
+	cnt_us=$((cnt_us + 1))
 	echo $v
 	sum_us=$((sum_us + v))
 done
@@ -38,12 +59,8 @@ echo "sum of cnt_us = $cnt_us"
 echo "sum of cpu_us = $sum_us"
 echo "avg of cpu_us = $avg_us"
 
-first_sy=true
-second_sy=true
-
 for v in $cpu_sy
 do
-	cnt_sy=$((cnt_sy + 1))
 	
 	if $first_sy; then
 		first_sy=false
@@ -53,6 +70,8 @@ do
 		second_sy=false
 		continue
 	fi
+
+	cnt_sy=$((cnt_sy + 1))
 	echo $v
 	sum_sy=$((sum_sy + v))
 done
@@ -64,6 +83,88 @@ echo "sum of cnt_sy = $cnt_sy"
 echo "sum of cpu_sy = $sum_sy"
 echo "sum of avg_sy = $avg_sy"
 
-avg_total=$(awk "BEGIN {printf \"%.2f\", ($sum_us + $sum_sy)/($cnt_us + $cnt_sy)}")
+cpu_avg_total=$(awk "BEGIN {printf \"%.2f\", ($sum_us + $sum_sy)/($cnt_us + $cnt_sy)}")
 
-echo "avg of cpu usage = $avg_total%"
+echo "avg of cpu usage = $cpu_avg_total%"
+
+### Memory Usage
+
+total_mem=$(free -k | awk '/Mem:/ {print $2}')
+
+
+for v in $mem_free
+do
+
+        if $first_free; then
+                first_free=false
+                continue
+        fi
+        if $second_free; then
+                second_free=false
+                continue
+        fi
+
+        cnt_free=$((cnt_free + 1))
+        echo $v
+        sum_free=$((sum_free + v))
+done
+
+avg_free=$(awk "BEGIN {printf \"%.2f\", $sum_free/$cnt_free}")
+
+echo "sum of cnt_free = $cnt_free"
+echo "sum of cpu_free = $sum_free"
+echo "sum of avg_free = $avg_free"
+
+for v in $mem_buff
+do
+
+        if $first_buff; then
+                first_buff=false
+                continue
+        fi
+        if $second_buff; then
+                second_buff=false
+                continue
+        fi
+
+        cnt_buff=$((cnt_buff + 1))
+        echo $v
+        sum_buff=$((sum_buff + v))
+done
+
+avg_buff=$(awk "BEGIN {printf \"%.2f\", $sum_buff/$cnt_buff}")
+
+echo "sum of cnt_buff = $cnt_buff"
+echo "sum of cpu_buff = $sum_buff"
+echo "sum of avg_buff = $avg_buff"
+
+for v in $mem_cache
+do
+
+        if $first_cache; then
+                first_cache=false
+                continue
+        fi
+        if $second_cache; then
+                second_cache=false
+                continue
+        fi
+
+        cnt_cache=$((cnt_cache + 1))
+        echo $v
+        sum_cache=$((sum_cache + v))
+done
+
+avg_cache=$(awk "BEGIN {printf \"%.2f\", $sum_cache/$cnt_cache}")
+
+echo "sum of cnt_cache = $cnt_cache"
+echo "sum of cpu_cache = $sum_cache"
+echo "sum of avg_cache = $avg_cache"
+
+mem_avg_total=$(awk "BEGIN {printf \"%.2f\", (($total_mem - $avg_free - $avg_buff - $avg_cache) / $total_mem) * 100 }")
+echo "mem avg usage = $mem_avg_total%" 
+
+#echo $mem_free
+#echo $mem_buff
+#echo $mem_cache
+echo $total_mem
